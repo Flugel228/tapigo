395736f94a3c2d5d13aebcb842e6d3d5967c839e
Flugel228 <imflugel@gmail.com>
2023-10-25 01:48:59 +0300

    feat: add list of posts page

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h1>About page</h1>
+      <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ipsa quis saepe sunt totam. A alias aliquam aut, culpa debitis enim eum magnam nam odit officia, praesentium quia quisquam reiciendis.</span><span>Alias aliquam commodi consectetur consequatur consequuntur dolore eius, est et harum hic incidunt, inventore ipsa magni maiores mollitia nam necessitatibus nisi non odio perspiciatis placeat possimus qui recusandae repudiandae voluptatum!</span><span>Aspernatur, culpa debitis delectus deserunt earum eum impedit in ipsam maxime nesciunt rerum saepe voluptate voluptatum? Aut consequatur dolores facilis harum mollitia similique sint voluptatibus! Beatae distinctio eius explicabo impedit!</span><span>A amet animi asperiores assumenda beatae consectetur cupiditate dignissimos dolores ea esse fugiat inventore odio officia officiis placeat possimus, quidem quo quos unde voluptatum. Ab esse maiores obcaecati officia voluptates?</span><span>Exercitationem nobis sit tempore! Adipisci, aliquam aut dolor error facilis hic illum ipsa iusto maxime molestiae porro praesentium quibusdam quos repellat reprehenderit similique sit, sunt tempore. Fugit natus officiis quo?</span><span>Architecto asperiores atque, cumque dicta earum eius eos esse facere fugiat fugit illo iste magni molestias nemo non officiis quos, reiciendis repellat repellendus, repudiandae rerum similique sunt suscipit ullam voluptas.</span><span>Amet consequatur cupiditate explicabo, itaque modi nobis perspiciatis placeat ratione repellendus. Asperiores dolorem error ipsam magnam minima nobis velit voluptates. Corporis debitis error maiores nulla qui totam voluptatibus! Doloribus, obcaecati.</span><span>Ab, consectetur cum dolore doloremque doloribus exercitationem fugiat harum modi molestias nam nemo nesciunt nulla officiis omnis pariatur porro possimus quia, quisquam ratione sequi similique tempora tempore tenetur voluptas voluptates?</span><span>Enim nam, natus nemo nostrum nulla quae quos reiciendis repellat soluta tenetur. Ab, alias debitis dolores laudantium neque nesciunt numquam obcaecati perferendis quidem recusandae repellat repellendus sint temporibus, veritatis vero.</span><span>Fuga nostrum pariatur placeat. Alias amet architecto at atque dicta dolore eaque eius facere, hic illum incidunt inventore iure modi, numquam obcaecati placeat quam quibusdam quis quisquam quo reiciendis rerum?</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+<style scoped>
+
+</style>
